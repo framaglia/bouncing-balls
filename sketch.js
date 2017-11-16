d73dccd11de0ed48ea7cc09ba822119ee09618a8
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-11-16 14:27:36 +0100

    adding sources

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
-var force;
 var balls = [];
 var controller;
 var frame;
 
 function setup() {
 	createCanvas(windowWidth - 20, windowHeight - 20);
-	forceRHand = createVector(0, -5);
-	forceLHand = createVector(0, -5);
-	wind = createVector(10, 0);
+	forceRHand = createVector(0, -150);
+	forceLHand = createVector(0, -150);
 	forceRKey = createVector(100, 0);
 	forceLKey = createVector(-100, 0);
 	forceUKey = createVector(0, -300);
@@ -18,23 +16,22 @@ function setup() {
 	}
 }
  
-
 function draw() {
 	background(160);
 	frame = controller.frame();
 	for(var i = balls.length - 1; i > 0; i--) {
+      if (frame.hands.length == 1) {
+         balls[i].applyForce(forceHand);
+      } else if (frame.hands.length == 2) {
+         balls[i].applyForce(forceHand);
+         balls[i].applyForce(forceHand);
+      }
 		var gravity = createVector(0, balls[i].mass);
 		balls[i].applyForce(gravity);
 		balls[i].update();
 		balls[i].display();
 		balls[i].bounce();
 	}
-}
-
-function mousePressed() {
-	for(var i = balls.length - 1; i > 0; i--) {
-		balls[i].applyForce(wind);
-	}  
 }
 
 function keyPressed() {
