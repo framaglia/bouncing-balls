af987a91a657ba05cecac6ac29572c38249e5777
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-11-16 16:10:09 +0100

    added stylesheet

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var controller;
 var frame;
 
 function setup() {
-	createCanvas(windowWidth/2, windowHeight/2);
+	createCanvas(windowWidth * 0.7, windowHeight * 0.7);
 	forceRHand = createVector(0, -150);
 	forceLHand = createVector(0, -150);
 	forceRKey = createVector(100, 0);
